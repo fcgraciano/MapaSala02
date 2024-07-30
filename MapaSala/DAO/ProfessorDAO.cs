080d18b4a607b8cff7166fd67a663f636f0b419b
Fernando <fcgraciano@gmail.com>
2024-07-30 10:51:03 -0300

    Inicio da conexão com banco de dados

---
+++ MapaSala/DAO/ProfessorDAO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using Model.Entitidades;
+
+namespace MapaSala.DAO
+{
+    public class ProfessorDAO
+    {
+        private string LinhaConexao = "Server=LS05MPF;Database=AULA_DS;User Id=sa;Password=admsasql;";
+        private SqlConnection Conexao;
+        public ProfessorDAO()
+        {
+            Conexao = new SqlConnection(LinhaConexao);
+        }
+
+        public void Inserir(ProfessoresEntidade professor)
+        {
+            Conexao.Open();
+            string query = "Insert into Professores "
+        }
+
+        
+
+    }
+}
