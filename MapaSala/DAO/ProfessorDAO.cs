86b02315c6ee9f848732028741bdfe8427d1f1ab
Fernando <fcgraciano@gmail.com>
2024-08-05 13:43:27 -0300

    Metodo Inserir do professor DAO

--- MapaSala/DAO/ProfessorDAO.cs
+++ MapaSala/DAO/ProfessorDAO.cs
@@ -21,7 +21,17 @@ namespace MapaSala.DAO
         public void Inserir(ProfessoresEntidade professor)
         {
             Conexao.Open();
-            string query = "Insert into Professores "
+            string query = "Insert into Professores (Nome , Apelido) Values (@nome, @apelido) ";
+            SqlCommand comando = new SqlCommand(query,Conexao);
+            SqlParameter parametro1 = new SqlParameter("@nome", professor.Nome);
+            SqlParameter parametro2 = new SqlParameter("@apelido", professor.Apelido);
+            comando.Parameters.Add(parametro1);
+            comando.Parameters.Add(parametro2);
+            comando.ExecuteNonQuery();
+            Conexao.Close();
+
+
+
         }
 
         
