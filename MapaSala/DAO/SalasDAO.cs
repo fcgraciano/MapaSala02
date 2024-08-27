dc29223f17bca82b0fac92640827c6a18f81b6f4
Fernando <fcgraciano@gmail.com>
2024-08-27 20:42:45 -0300

    Salas Dao parcial

---
+++ MapaSala/DAO/SalasDAO.cs
@@ -0,0 +1,97 @@
+﻿using Model.Entitidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaSala.DAO
+{
+    public class SalasDAO
+    {
+        private string LinhaConexao = "Server=LS05MPF;Database=AULA_DS;User Id=sa;Password=admsasql;";
+        private SqlConnection Conexao;
+        public SalasDAO()
+        {
+            Conexao = new SqlConnection(LinhaConexao);
+        }
+
+        public void Inserir(SalasEntidade objeto)
+        {
+            Conexao.Open();
+            string query = "Insert into Salas (Nome , NumeroComputadores, NumeroCadeiras, IsLab, Disponivel) Values (@nome, @numerocomputadores, @numerocadeiras, @islab, @disponivel) ";
+            SqlCommand comando = new SqlCommand(query, Conexao);
+
+            SqlParameter parametro1 = new SqlParameter("@nome", objeto.Nome);
+            SqlParameter parametro2 = new SqlParameter("@numerocomputadores", objeto.NumeroComputadores);
+            SqlParameter parametro3 = new SqlParameter("@numerocadeiras", objeto.NumeroCadeiras);
+            SqlParameter parametro4 = new SqlParameter("@islab", objeto.IsLab);
+            SqlParameter parametro5 = new SqlParameter("@disponivel", objeto.Disponivel);
+            comando.Parameters.Add(parametro1);
+            comando.Parameters.Add(parametro2);
+            comando.Parameters.Add(parametro3);
+            comando.Parameters.Add(parametro4);
+            comando.Parameters.Add(parametro5);
+
+            comando.ExecuteNonQuery();
+            Conexao.Close();
+
+        }
+
+        public DataTable PreencherComboBox()
+        {
+            DataTable dataTable = new DataTable();
+
+            string query = "SELECT Id, Nome FROM Salas";
+
+            using (SqlConnection connection = new SqlConnection(LinhaConexao))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+
+                try
+                {
+                    // Preenche o DataTable com os dados da consulta
+                    adapter.Fill(dataTable);
+                }
+                catch (Exception ex)
+                {
+                    // Lida com erros, se necessário
+                    throw new Exception("Erro ao acessar os dados: " + ex.Message);
+                }
+            }
+
+            return dataTable;
+        }
+
+        public DataTable ObterDisciplinas()
+        {
+            DataTable dt = new DataTable();
+            Conexao.Open();
+            string query = "SELECT Nome , Turno, Sigla, Ativo FROM Cursos Order by Id desc";
+            SqlCommand comando = new SqlCommand(query, Conexao);
+
+            SqlDataReader Leitura = comando.ExecuteReader();
+
+            foreach (var atributos in typeof(DisciplinaEntidade).GetProperties())
+            {
+                dt.Columns.Add(atributos.Name);
+            }
+
+            if (Leitura.HasRows)
+            {
+                while (Leitura.Read())
+                {
+                    ProfessoresEntidade p = new ProfessoresEntidade();
+                    p.Id = Convert.ToInt32(Leitura[0]);
+                    p.Nome = Leitura[1].ToString();
+                    p.Apelido = Leitura[2].ToString();
+                    dt.Rows.Add(p.Linha());
+                }
+            }
+            Conexao.Close();
+            return dt;
+        }
+    }
+}
