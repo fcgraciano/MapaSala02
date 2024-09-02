92103db9197a264c93a988adbcd4719fdbfec0ce
fcgraciano <fcgraciano@gmail.com>
2024-09-02 13:46:53 -0300

    CursoDisciplina DAO

--- MapaSala/Formularios/FrmCursoDisciplina.Designer.cs
+++ MapaSala/Formularios/FrmCursoDisciplina.Designer.cs
@@ -29,22 +29,14 @@ namespace MapaSala.Formularios
         /// </summary>
         private void InitializeComponent()
         {
-            this.lsDisciplinas = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.cbxCursos = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.btnAdicionar = new System.Windows.Forms.Button();
             this.dtCursoDisciplina = new System.Windows.Forms.DataGridView();
+            this.cbDisciplinas = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dtCursoDisciplina)).BeginInit();
             this.SuspendLayout();
-            // 
-            // lsDisciplinas
-            // 
-            this.lsDisciplinas.FormattingEnabled = true;
-            this.lsDisciplinas.Location = new System.Drawing.Point(12, 29);
-            this.lsDisciplinas.Name = "lsDisciplinas";
-            this.lsDisciplinas.Size = new System.Drawing.Size(120, 95);
-            this.lsDisciplinas.TabIndex = 0;
             // 
             // label1
             // 
@@ -89,17 +81,25 @@ namespace MapaSala.Formularios
             this.dtCursoDisciplina.Size = new System.Drawing.Size(453, 150);
             this.dtCursoDisciplina.TabIndex = 5;
             // 
+            // cbDisciplinas
+            // 
+            this.cbDisciplinas.FormattingEnabled = true;
+            this.cbDisciplinas.Location = new System.Drawing.Point(16, 31);
+            this.cbDisciplinas.Name = "cbDisciplinas";
+            this.cbDisciplinas.Size = new System.Drawing.Size(121, 21);
+            this.cbDisciplinas.TabIndex = 6;
+            // 
             // FrmCursoDisciplina
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 310);
+            this.Controls.Add(this.cbDisciplinas);
             this.Controls.Add(this.dtCursoDisciplina);
             this.Controls.Add(this.btnAdicionar);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.cbxCursos);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.lsDisciplinas);
             this.Name = "FrmCursoDisciplina";
             this.Text = "FrmCursoDisciplina";
             ((System.ComponentModel.ISupportInitialize)(this.dtCursoDisciplina)).EndInit();
@@ -109,12 +109,11 @@ namespace MapaSala.Formularios
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox lsDisciplinas;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cbxCursos;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnAdicionar;
         private System.Windows.Forms.DataGridView dtCursoDisciplina;
+        private System.Windows.Forms.ComboBox cbDisciplinas;
     }
 }
