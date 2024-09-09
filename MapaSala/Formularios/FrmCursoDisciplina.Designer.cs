75e88b2e9ef17df4bd9054127ac119ffa5f6b9cf
Fernando <fcgraciano@gmail.com>
2024-09-09 13:51:47 -0300

    Adicionar cursoDisciplinadao inserir

--- MapaSala/Formularios/FrmCursoDisciplina.Designer.cs
+++ MapaSala/Formularios/FrmCursoDisciplina.Designer.cs
@@ -74,6 +74,7 @@ namespace MapaSala.Formularios
             this.btnAdicionar.TabIndex = 4;
             this.btnAdicionar.Text = "Adicionar";
             this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
             // 
             // dtCursoDisciplina
             // 
--- MapaSala/Formularios/FrmCursoDisciplina.cs
+++ MapaSala/Formularios/FrmCursoDisciplina.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 using MapaSala.DAO;
+using Model.Entitidades;
 
 namespace MapaSala.Formularios
 {
@@ -7,7 +9,7 @@ namespace MapaSala.Formularios
     {
         DisciplinaDAO disciplinaDAO = new DisciplinaDAO();
         CursoDAO cursoDAO = new CursoDAO();
-
+       
         public FrmCursoDisciplina()
         {
             InitializeComponent();
@@ -19,6 +21,23 @@ namespace MapaSala.Formularios
             cbxCursos.DataSource = cursoDAO.PreencherComboBox();
             cbxCursos.DisplayMember = "Nome";
             cbxCursos.ValueMember = "Id";
+        }
+
+        private void btnAdicionar_Click(object sender, System.EventArgs e)
+        {
+
+            CursoDisciplinaDAO cursoDisciplinaDao = new CursoDisciplinaDAO();
+
+            CursoDisciplinaEntidade entidade = new CursoDisciplinaEntidade();
+
+            entidade.CursoId = Convert.ToInt32(cbxCursos.SelectedValue);
+            entidade.DisciplinaId = Convert.ToInt32(cbDisciplinas.SelectedValue);
+            entidade.Periodo = cbPeriodos.SelectedValue.ToString();
+
+            cursoDisciplinaDao.Inserir(entidade);
+
+
+
         }
     }
 }
